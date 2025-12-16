23ecf8455c67c3d9999dca053da49c68c6d519ff
OrichalcosShunnoros <c3146092091@gmail.com>
2025-12-16 05:28:04 -0500

    base code was creates for some grades widgets | next: apply dry and modularized and optimized code to implement lógic with data files in json

--- lib/data/grade_menu_options.dart
+++ lib/data/grade_menu_options.dart
@@ -1,29 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:lqh_app/models/grade_menu_option.dart';
+
 import 'package:lqh_app/screens/views/grades/gup1.dart';
 import 'package:lqh_app/screens/views/grades/gup2.dart';
 import 'package:lqh_app/screens/views/grades/gup3.dart';
+import 'package:lqh_app/screens/views/grades/gup4.dart';
+import 'package:lqh_app/screens/views/grades/gup5.dart';
+import 'package:lqh_app/screens/views/grades/gup6.dart';
+import 'package:lqh_app/screens/views/grades/gup7.dart';
+import 'package:lqh_app/screens/views/grades/gup8.dart';
+import 'package:lqh_app/screens/views/grades/gup9.dart';
+import 'package:lqh_app/screens/views/grades/gup10.dart';
+import 'package:lqh_app/screens/views/grades/dan1.dart';
+import 'package:lqh_app/screens/views/grades/dan2.dart';
+import 'package:lqh_app/screens/views/grades/dan3.dart';
+import 'package:lqh_app/screens/views/grades/dan4.dart';
+import 'package:lqh_app/screens/views/grades/dan5.dart';
 
 final List<GradeMenuOption> gradeMenuOptions = [
   GradeMenuOption(
     grade: "Blanco",
     romanized: "(Hayan tti)",
     hangul: "하얀 띠",
-    screen: Gup1(),
+    screen: Gup10(),
     color: Colors.white,
   ),
   GradeMenuOption(
     grade: "Amarillo",
     romanized: "(Noran tti)",
     hangul: "노란 띠",
-    screen: Gup2(),
+    screen: Gup9(),
     color: Colors.yellow,
   ),
   GradeMenuOption(
     grade: "Naranja",
-    romanized: "(juhwang tti)",
+    romanized: "(Juhwang tti)",
     hangul: "주황 띠",
-    screen: Gup3(),
+    screen: Gup8(),
     color: Colors.orange,
+  ),
+  GradeMenuOption(
+    grade: "Verde",
+    romanized: "(Chorok tti)",
+    hangul: "초록 띠",
+    screen: Gup7(),
+    color: Colors.green,
+  ),
+  GradeMenuOption(
+    grade: "Azul",
+    romanized: "(Paran tti)",
+    hangul: "파란 띠",
+    screen: Gup6(),
+    color: Colors.blue,
+  ),
+  GradeMenuOption(
+    grade: "Púrpura",
+    romanized: "(Bora tti)",
+    hangul: "보라 띠",
+    screen: Gup5(),
+    color: Colors.deepPurple,
+  ),
+  GradeMenuOption(
+    grade: "Rojo",
+    romanized: "(Ppalgan tti)",
+    hangul: "빨간 띠",
+    screen: Gup4(),
+    color: Colors.red,
+  ),
+  GradeMenuOption(
+    grade: "Rojo Marrón",
+    romanized: "(Ppalgan gal tti)",
+    hangul: "빨간 갈 띠",
+    screen: Gup3(),
+    color: Colors.brown,
+  ),
+  GradeMenuOption(
+    grade: "Marrón",
+    romanized: "(Gal tti)",
+    hangul: "갈 띠",
+    screen: Gup2(),
+    color: Colors.brown.shade700,
+  ),
+  GradeMenuOption(
+    grade: "Marrón Negro",
+    romanized: "(Gal geomjeong tti)",
+    hangul: "갈 검정 띠",
+    screen: Gup1(),
+    color: Colors.black87,
+  ),
+  GradeMenuOption(
+    grade: "Negro 1 Dan",
+    romanized: "(Geomjeong 1 dan)",
+    hangul: "검정 1단",
+    screen: Dan1(),
+    color: Colors.black,
+  ),
+  GradeMenuOption(
+    grade: "Negro 2 Dan",
+    romanized: "(Geomjeong 2 dan)",
+    hangul: "검정 2단",
+    screen: Dan2(),
+    color: Colors.black,
+  ),
+  GradeMenuOption(
+    grade: "Negro 3 Dan",
+    romanized: "(Geomjeong 3 dan)",
+    hangul: "검정 3단",
+    screen: Dan3(),
+    color: Colors.black,
+  ),
+  GradeMenuOption(
+    grade: "Negro 4 Dan",
+    romanized: "(Geomjeong 4 dan)",
+    hangul: "검정 4단",
+    screen: Dan4(),
+    color: Colors.black,
+  ),
+  GradeMenuOption(
+    grade: "Negro 5 Dan",
+    romanized: "(Geomjeong 5 dan)",
+    hangul: "검정 5단",
+    screen: Dan5(),
+    color: Colors.black,
   ),
 ];
